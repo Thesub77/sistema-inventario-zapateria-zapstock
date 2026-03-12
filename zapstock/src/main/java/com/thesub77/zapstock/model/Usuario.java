f507062eae41e3ac8b8b612dc95212d520d6d42f
Thesub77 <d7fquiroz@gmail.com>
2026-03-11 21:17:36 -0600

    feat: Se completo el modelo de Usuario y su CRUD

--- zapstock/src/main/java/com/thesub77/zapstock/model/Usuario.java
+++ zapstock/src/main/java/com/thesub77/zapstock/model/Usuario.java
@@ -1,12 +1,143 @@
 /*
  * @author Douglas Quiroz (@thesub77)
- * @proyect_name Zapstock
+ * @project_name Zapstock
  */
 package com.thesub77.zapstock.model;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  *
  */
+@Entity
+@Table(name = "Usuario")
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUsuario")
+    private Long idUsuario;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<RolPrivilegio> rolprivilegio = new ArrayList<>();
+
+    @Column(name = "codigoUsuario", unique = true, length = 16, nullable = false)
+    private String codigoUsuario;
+
+    @Column(name = "nombreUsuario", length = 16, nullable = false)
+    private String nombreUsuario;
+
+    @Column(name = "apellidoUsuario", length = 16, nullable = false)
+    private String apellidoUsuario;
+
+    @Column(name = "correoElectronico", unique = true, length = 64, nullable = false)
+    private String correoElectronico;
+
+    @Column(name = "contrasena", length = 64, nullable = false)
+    private String contrasena;
+
+    @Column(name = "estado", nullable = false)
+    private boolean estado;
+
+    // Constructor vacio para instancias
+    public Usuario() {
+
+    }
+
+    // Constructor completo para entrada manual
+    public Usuario(String codigoUsuario, String nombreUsuario, String apellidoUsuario, String correoElectronico, String contrasena, boolean estado) {
+        this.codigoUsuario = codigoUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+        this.estado = estado;
+    }
+
+    // Getters y Setters para interaccion con los atributos
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public List<RolPrivilegio> getRolprivilegio() {
+        return rolprivilegio;
+    }
+
+    public void setRolprivilegio(List<RolPrivilegio> rolprivilegio) {
+        this.rolprivilegio = rolprivilegio;
+    }
+
+    public String getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(String codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getApellidoUsuario() {
+        return apellidoUsuario;
+    }
+
+    public void setApellidoUsuario(String apellidoUsuario) {
+        this.apellidoUsuario = apellidoUsuario;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    // Para comparaciones logicas y caching
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(idUsuario, usuario.idUsuario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idUsuario);
+    }
+
 }
--- zapstock/src/main/java/com/thesub77/zapstock/repository/UsuarioRepository.java
+++ zapstock/src/main/java/com/thesub77/zapstock/repository/UsuarioRepository.java
@@ -1,12 +1,143 @@
 /*
  * @author Douglas Quiroz (@thesub77)
- * @proyect_name Zapstock
+ * @project_name Zapstock
  */
 package com.thesub77.zapstock.repository;
+
+import com.thesub77.zapstock.model.RolPrivilegio;
+import com.thesub77.zapstock.model.Usuario;
+import com.thesub77.zapstock.util.DatabaseUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.NoResultException;
+import java.util.List;
 
 /**
  *
  */
 public class UsuarioRepository {
-    
+
+    public void guardarUsuario(Usuario usuario) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(usuario);
+            em.getTransaction().commit();
+            System.out.println("Se ha creado el usuario bajo el correo " + usuario.getCorreoElectronico());
+
+        } catch (Exception ex) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw new RuntimeException("Error al crear usuario: " + ex.getMessage(), ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void actualizarUsuario(Usuario usuarioActualizado) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            // Verificar existencia y estado antes de actualizar
+            Usuario verificarExistencia = em.find(Usuario.class, usuarioActualizado.getIdUsuario());
+            if (verificarExistencia == null || !verificarExistencia.isEstado()) {
+                throw new EntityNotFoundException("Error al actualizar: Usuario no encontrado");
+            }
+
+            em.merge(usuarioActualizado);
+            em.getTransaction().commit();
+            System.out.println("Se ha actualizado el usuario " + usuarioActualizado.getCorreoElectronico());
+
+        } catch (Exception ex) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw new RuntimeException("Error al actualizar usuario: " + ex.getMessage(), ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public Usuario buscarUsuarioPorId(Long idUsuario) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+
+        try {
+            Usuario usuario = em.find(Usuario.class, idUsuario);
+
+            if (usuario == null || !usuario.isEstado()) {
+                throw new EntityNotFoundException("Usuario con ID " + idUsuario + " no encontrado");
+            }
+
+            return usuario;
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public Usuario buscarUsuarioPorCodigo(String codigoUsuario) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+
+        try {
+            return em.createQuery("SELECT u FROM Usuario u WHERE u.codigoUsuario = :codigoUsuario AND u.estado = true", Usuario.class)
+                    .setParameter("codigoUsuario", codigoUsuario)
+                    .getSingleResult();
+
+        } catch (NoResultException nre) {
+            throw new EntityNotFoundException("Usuario " + codigoUsuario + " no encontrado " + nre.getMessage() + " " + nre);
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public List<Usuario> listarUsuarios() {
+        EntityManager em = DatabaseUtil.getEntityManager();
+
+        try {
+            return em.createQuery("SELECT u FROM Usuario u WHERE u.estado = true", Usuario.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void borrarUsuario(String codigoUsuario) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            Usuario usuario = em.createQuery("SELECT u FROM Usuario u WHERE u.codigoUsuario = :codigoUsuario AND u.estado = true", Usuario.class)
+                    .setParameter("codigoUsuario", codigoUsuario)
+                    .getSingleResult();
+
+            usuario.setEstado(false);
+
+            for (RolPrivilegio rp : usuario.getRolprivilegio()) {
+                rp.setEstado(false);
+            }
+
+            em.getTransaction().commit();
+
+            System.out.println("Se ha eliminado el usuario: " + codigoUsuario);
+
+        } catch (NoResultException nre) {
+            throw new EntityNotFoundException("No se ha encontrado el usuario" + nre.getMessage(), nre);
+        } catch (Exception ex) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+        } finally {
+            em.close();
+        }
+
+    }
+
 }
